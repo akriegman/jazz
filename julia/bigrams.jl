e43f347715ee7894978dc8264f6a62ea876b55bb
Aaron Kriegman <aaronkplus2@gmail.com>
2022-05-10 21:55:31 +0100

    Data analysis

---
+++ julia/bigrams.jl
@@ -0,0 +1,17 @@
+using DelimitedFiles
+using Chain
+
+identdefault(x::Number) = x
+identdefault(x) = 0
+
+table = readdlm("bigrams.txt")
+freq = @chain table[:, 2:end] identdefault.() exp.()
+letters = table[:, 1]
+
+fingers = [indexin(f, letters) for f in [["a", "q"], ["w", "s"], ["d", "r"], ["f", "t", "g", "v", "c"],
+           ["h", "n", "m", "u"], ["k", "i", "y"], ["l", "o"]]]
+           
+overlap(f, g) = sum(freq[f, g]) + sum(freq[g, f])
+
+costs(ls...) = [overlap(indexin(ls, letters), f) for f in fingers]
+enemies(l) = @chain indexin([l], letters)[1] freq[_, :] + freq[:, _] [letters _][sortperm(_), :]
